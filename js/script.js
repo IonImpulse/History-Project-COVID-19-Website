95371cec9379645e50516d3395e4ad5ffe9b21a0
Ethan Vazquez <edv121@outlook.com>
2020-03-23 17:44:18 -0700

    finished v0.1 of website

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 function returnResults() {
     var sex = document.forms["mainform"]["sex"].value;
     var age = document.forms["mainform"]["age"].value;
-    if (isInt(age) == false) {
-        alert("Please input a number for age");
+    if (isInt(age) == false || sex == "") {
+        alert("Please fill in both fields");
         return false;
     } else {
-        if (parseInt(age) > 105) {
-            alert("Please input an age lower then 105");
+        if (parseInt(age) > 104 || parseInt(age) < 1) {
+            alert("Please input an age lower then 105 and greater then 0");
             return false;
         } else {
+            console.log(sex, age);
             document.getElementById("results").innerHTML = getResults(sex, age);            
         }
     }
@@ -21,47 +22,44 @@ function isInt(value) {
     x = parseFloat(value);
     return (x | 0) === x;
 }
+
+
 function getResults(sex, age) {
-    var data = loadFile(sex);
-    var hosp_num = data[0][age];
-    var mortal_num = data[1][age];
-    
-
-    hosp_num = Math.round(hosp_num * 100);
-    mortal_num = Math.round(mortal_num * 100);
-
-    console.log("Relative chance of hospitalization: " + toString(hosp_num) + "\nRelative change of death: " + toString(mortal_num));
-    return "Relative chance of hospitalization: " + toString(hosp_num) + "\nRelative change of death: " + toString(mortal_num);
-}
-
-function loadFile(sex) {
     var name = "";
-    self.hosp = [];
-    self.mortal = [];
+    var hosp = [];
+    var mortal = [];
+    
     if (sex == "male") {    
-        name = "data/regressed_male_calculator_data.csv";
+        name = "https://ionimpulse.github.io/History-Project-COVID-19-Website/data/regressed_male_calculator_data.csv";
     } else {
-        name = "data/regressed_female_calculator_data.csv";
+        name = "https://ionimpulse.github.io/History-Project-COVID-19-Website/data/regressed_female_calculator_data.csv";
     }
 
     $.ajax({
         type: "GET",
         url: name,
         dataType: "text",
-        success: function(data) {processData(data);}
+        success: function(data) {processData(data);},
+        async: false
     });
 
     // Let's process the data from the data file
     function processData(data) {
         var lines = data.split(/\r\n|\n/);
 
-        for (var j=0; j<lines.length; j++) {
-        var values = lines[j].split(','); // Split up the comma seperated values
-           self.hosp.push(parseFloat(values[0])); 
-           self.mortal.push(parseFloat(values[1]));
+        for (var j=0; j<lines.length-1; j++) {
+            var values = lines[j].split(','); // Split up the comma seperated values
+            hosp.push(parseFloat(values[0])); 
+            mortal.push(parseFloat(values[1]));
         }
-
-        console.log(data);
     }
-    return [self.hosp, self.mortal];
+
+    var hosp_num = hosp[parseInt(age)];
+    var mortal_num = mortal[parseInt(age)];
+
+    hosp_num = Math.round(hosp_num * 100);
+    mortal_num = Math.round(mortal_num * 100);
+
+    console.log("Relative chance of hospitalization: " + hosp_num.toString() + "%\nRelative chance of death: " + mortal_num.toString() + "%");
+    return "Relative chance of hospitalization: " + hosp_num.toString() + "%<br>Relative chance of death: " + mortal_num.toString() + "%";
 }
